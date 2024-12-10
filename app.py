c276ad27ac564feac970bec276b532c68db98f9d
aysickrari <joshua.rufino07@gmail.com>
2024-12-10 15:21:06 -0800

    complete working application

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import altair as alt
 
 df=pd.read_csv(r'C:/Users/joshu/OneDrive/Documents/S4Project/S4-Project/vehicles_us.csv')
 
