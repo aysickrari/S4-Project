05d11ebdaac1cc4436b679bc00aafbfc866a6091
aysickrari <joshua.rufino07@gmail.com>
2024-12-10 18:56:30 -0800

    test

--- app.py
+++ app.py
@@ -21,13 +21,12 @@ st.header('Data viewer')
 st.dataframe(df)
 
 # histogram of prices by manufacturer
-st.subheader("Distribution of Car Prices")
+st.subheader("Distribution of Car Prices By Manufacturer")
 fig_manufacturer_hist = px.histogram(
     df,
     x='price',
     color='manufacturer',
     barmode='overlay',
-    title='Price Distribution by Manufacturer',
     labels={'price': 'Price ($)', 'manufacturer': 'Manufacturer'},
     nbins=50,
     color_discrete_sequence=px.colors.qualitative.Set3
@@ -35,6 +34,21 @@ fig_manufacturer_hist = px.histogram(
 # Limit x-axis range to 100,000
 fig_manufacturer_hist.update_layout(xaxis_range=[0, 100000])
 st.plotly_chart(fig_manufacturer_hist)
+
+# Histogram of Model Year
+st.subheader("Distribution of Car Model Years By Manufacturer")
+fig_year_hist = px.histogram(
+    df, 
+    x='model_year', 
+    labels={'model_year': 'Model Year'},
+    color='manufacturer',
+    barmode='overlay',
+    nbins=30,
+    color_discrete_sequence=px.colors.qualitative.Set3
+)
+# Limit x-axis range from 1960 to 2024
+fig_year_hist.update_layout(xaxis_range=[1960, 2020])
+st.plotly_chart(fig_year_hist)
 
 # histogram distribution of vehicle types by the manufacturer
 st.header('Vehicle types by manufacturer')
